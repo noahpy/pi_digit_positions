e2807a657a555c6e0706213177c3f975f8dea380
noahpy <noschl@proton.me>
2025-07-14 20:04:00 +0200

    implement additional features
    - command-line argument
    - searching for patterns starting with 0
    - printing beginning of line

--- calc_position.py
+++ calc_position.py
@@ -1,10 +1,22 @@
 import re
+import sys
+
+def get_number_at_pos(text, pos):
+    return text[pos]
+
+def get_numbers_at_pos(text, pos, length = 5):
+    return text[pos:pos + 5]
+
 
 with open("pi_million", "r") as f:
     text = f.read()
 
 try:
-    number = int(input("Search for: "))
+    if len(sys.argv) > 1:
+        number = sys.argv[1]
+    else:
+        number = input("Search for: ")
+    _ = int(number)
 except ValueError:
     print("Input has to be a number")
     exit()
@@ -12,7 +24,9 @@ except ValueError:
 match = re.search(rf"{number}", text)
 
 if match:
-    i = int(match.start(0)) + 1
+    found_pos = int(match.start(0)) + 1
+    i = found_pos
+    print("Number found at position " + str(i), end=", ")
 else:
     print("Number could not be found")
     exit()
@@ -24,7 +38,7 @@ first_line = 36
 first_page = 40 * 52
 
 if i <= first_line:
-    print("Page 3, Line 1, Position " + str(i))
+    print("which is on Page 3, Line 1, Position " + str(i) + ".")
     exit(0)
 
 i -= first_line
@@ -32,7 +46,8 @@ i -= first_line
 if i <= first_page:
     line = i // num_cols + 2
     col = i % num_cols
-    print("Page 3, Line " + str(line) + ", Position " + str(col))
+    print("which is on Page 3, Line " + str(line) + ", Position " + str(col) + ".")
+    print("Line begins with " + get_numbers_at_pos(text, found_pos - col) + ".")
     exit(0)
 
 i -= first_page
@@ -43,4 +58,5 @@ i -= (page - 2) * num_rows * num_cols
 line = i // num_cols
 col = i % num_cols
 
-print("Page " + str(page + 2) + ", Line " + str(line) + ", Position " + str(col))
+print("which is on Page " + str(page + 2) + ", Line " + str(line) + ", Position " + str(col) + ".")
+print("Line begins with " + get_numbers_at_pos(text, found_pos - col) + ".")
